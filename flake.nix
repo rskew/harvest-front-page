dc9f5dcc111483583fea0adfc455bfa1f77755eb
Rowan Skewes <rowan.skewes@gmail.com>
2022-05-06 22:24:09 +1000

    Parameterise nixosModule by domain

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
       {
-        defaultPackage.x86_64-linux = self.packages.x86_64-linux.harvest-splash-page;
-        packages.x86_64-linux.harvest-splash-page =
+        defaultPackage.x86_64-linux = self.packages.x86_64-linux.harvest-front-page;
+        packages.x86_64-linux.harvest-front-page =
           pkgs.stdenv.mkDerivation {
-            name = "harvest-splash-page";
+            name = "harvest-front-page";
             src = ./assets;
             installPhase = ''
                 mkdir $out
@@ -17,12 +17,12 @@
             '';
             phases = [ "unpackPhase" "installPhase" ];
           };
-        nixosModule = { enableACME ? true, ACMEEmail, forceSSL ? true, ... }: {
+        nixosModule = { domain, enableACME ? true, ACMEEmail, forceSSL ? true, ... }: {
           services.nginx.enable = true;
-          services.nginx.virtualHosts."splash.castlemaineharvest.com.au" = {
+          services.nginx.virtualHosts.${domain} = {
             enableACME = enableACME;
             forceSSL = forceSSL;
-            root = self.packages.x86_64-linux.harvest-splash-page.out;
+            root = self.packages.x86_64-linux.harvest-front-page.out;
           };
           security.acme.email = if enableACME then ACMEEmail else null;
           security.acme.acceptTerms = if enableACME then true else false;
